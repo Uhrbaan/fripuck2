5022c363b5e42a5aee27825b2723816a88093018
Uhrbaan <uhrbaan@gmx.net>
2026-03-10 16:07:36 +0100

    feat: made uart prototype work Finally got interrupt UART with idle line detection working as a prototype. Will introduce into api.

--- firmware/controller-freertos/src/core/usart.c
+++ firmware/controller-freertos/src/core/usart.c
@@ -1,21 +1,21 @@
 /* USER CODE BEGIN Header */
 /**
- ******************************************************************************
- * @file    usart.c
- * @brief   This file provides code for the configuration
- *          of the USART instances.
- ******************************************************************************
- * @attention
- *
- * Copyright (c) 2026 STMicroelectronics.
- * All rights reserved.
- *
- * This software is licensed under terms that can be found in the LICENSE file
- * in the root directory of this software component.
- * If no LICENSE file comes with this software, it is provided AS-IS.
- *
- ******************************************************************************
- */
+  ******************************************************************************
+  * @file    usart.c
+  * @brief   This file provides code for the configuration
+  *          of the USART instances.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "usart.h"
@@ -25,7 +25,6 @@
 /* USER CODE END 0 */
 
 UART_HandleTypeDef huart3;
-DMA_HandleTypeDef hdma_usart3_rx;
 
 /* USART3 init function */
 
@@ -54,17 +53,18 @@ void MX_USART3_UART_Init(void)
   /* USER CODE BEGIN USART3_Init 2 */
 
   /* USER CODE END USART3_Init 2 */
+
 }
 
-void HAL_UART_MspInit(UART_HandleTypeDef *uartHandle)
+void HAL_UART_MspInit(UART_HandleTypeDef* uartHandle)
 {
 
   GPIO_InitTypeDef GPIO_InitStruct = {0};
-  if (uartHandle->Instance == USART3)
+  if(uartHandle->Instance==USART3)
   {
-    /* USER CODE BEGIN USART3_MspInit 0 */
+  /* USER CODE BEGIN USART3_MspInit 0 */
 
-    /* USER CODE END USART3_MspInit 0 */
+  /* USER CODE END USART3_MspInit 0 */
     /* USART3 clock enable */
     __HAL_RCC_USART3_CLK_ENABLE();
 
@@ -73,49 +73,30 @@ void HAL_UART_MspInit(UART_HandleTypeDef *uartHandle)
     PD8     ------> USART3_TX
     PD9     ------> USART3_RX
     */
-    GPIO_InitStruct.Pin = UART_TX_Pin | UART_RX_Pin;
+    GPIO_InitStruct.Pin = UART_TX_Pin|UART_RX_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_PULLUP;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
     GPIO_InitStruct.Alternate = GPIO_AF7_USART3;
     HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
-    /* USART3 DMA Init */
-    /* USART3_RX Init */
-    hdma_usart3_rx.Instance = DMA1_Stream1;
-    hdma_usart3_rx.Init.Channel = DMA_CHANNEL_4;
-    hdma_usart3_rx.Init.Direction = DMA_PERIPH_TO_MEMORY;
-    hdma_usart3_rx.Init.PeriphInc = DMA_PINC_DISABLE;
-    hdma_usart3_rx.Init.MemInc = DMA_MINC_ENABLE;
-    hdma_usart3_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
-    hdma_usart3_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
-    hdma_usart3_rx.Init.Mode = DMA_NORMAL;
-    hdma_usart3_rx.Init.Priority = DMA_PRIORITY_MEDIUM;
-    hdma_usart3_rx.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
-    if (HAL_DMA_Init(&hdma_usart3_rx) != HAL_OK)
-    {
-      Error_Handler();
-    }
-
-    __HAL_LINKDMA(uartHandle, hdmarx, hdma_usart3_rx);
-
     /* USART3 interrupt Init */
     HAL_NVIC_SetPriority(USART3_IRQn, 5, 0);
     HAL_NVIC_EnableIRQ(USART3_IRQn);
-    /* USER CODE BEGIN USART3_MspInit 1 */
+  /* USER CODE BEGIN USART3_MspInit 1 */
 
-    /* USER CODE END USART3_MspInit 1 */
+  /* USER CODE END USART3_MspInit 1 */
   }
 }
 
-void HAL_UART_MspDeInit(UART_HandleTypeDef *uartHandle)
+void HAL_UART_MspDeInit(UART_HandleTypeDef* uartHandle)
 {
 
-  if (uartHandle->Instance == USART3)
+  if(uartHandle->Instance==USART3)
   {
-    /* USER CODE BEGIN USART3_MspDeInit 0 */
+  /* USER CODE BEGIN USART3_MspDeInit 0 */
 
-    /* USER CODE END USART3_MspDeInit 0 */
+  /* USER CODE END USART3_MspDeInit 0 */
     /* Peripheral clock disable */
     __HAL_RCC_USART3_CLK_DISABLE();
 
@@ -123,16 +104,13 @@ void HAL_UART_MspDeInit(UART_HandleTypeDef *uartHandle)
     PD8     ------> USART3_TX
     PD9     ------> USART3_RX
     */
-    HAL_GPIO_DeInit(GPIOD, UART_TX_Pin | UART_RX_Pin);
-
-    /* USART3 DMA DeInit */
-    HAL_DMA_DeInit(uartHandle->hdmarx);
+    HAL_GPIO_DeInit(GPIOD, UART_TX_Pin|UART_RX_Pin);
 
     /* USART3 interrupt Deinit */
     HAL_NVIC_DisableIRQ(USART3_IRQn);
-    /* USER CODE BEGIN USART3_MspDeInit 1 */
+  /* USER CODE BEGIN USART3_MspDeInit 1 */
 
-    /* USER CODE END USART3_MspDeInit 1 */
+  /* USER CODE END USART3_MspDeInit 1 */
   }
 }
 
--- firmware/controller-freertos/src/main.c
+++ firmware/controller-freertos/src/main.c
@@ -35,7 +35,6 @@ int init_hardware(void)
     HAL_Init();
     SystemClock_Config();
     MX_GPIO_Init();
-    osKernelInitialize(); /* Call init function for freertos objects (in cmsis_os2.c) */
 
     MX_CAN1_Init();
     MX_TIM3_Init();
@@ -46,25 +45,73 @@ int init_hardware(void)
     return 0;
 }
 
-static int led_number = 0;
-void uart_action(uint8_t *data, uint16_t length)
+#define uart_data_size 128
+static uint8_t uart_data[uart_data_size] = {0};
+
+void buffer_is_full(uint16_t size)
 {
-    toggle_led(led_number);
-    led_number = (led_number + 1) & 0b111;
+    (void)uart_data;
+    (void)size;
 }
 
+void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
+{
+    if (huart->Instance == USART3)
+    {
+        toggle_led(LED_1);
+        buffer_is_full(Size);
+
+        // 'Size' is the number of bytes received until the IDLE event occurred.
+        // You can now process 'uart_data' directly as a full string!
+
+        // IMPORTANT: You must RE-START the listener immediately
+        HAL_UARTEx_ReceiveToIdle_IT(huart, uart_data, uart_data_size);
+    }
+}
+
+void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart)
+{
+    if (huart->Instance == USART3)
+    {
+        uint32_t err = HAL_UART_GetError(huart);
+
+        if (err & HAL_UART_ERROR_FE)
+        {
+            // Frame Error detected
+            toggle_led(LED_7); // Error LED
+        }
+
+        if (err & HAL_UART_ERROR_ORE)
+        {
+            // Overrun Error detected
+            __HAL_UART_CLEAR_OREFLAG(huart);
+        }
+
+        // The peripheral is now in an error state and stopped.
+        // We MUST clear the error and restart the listener.
+        huart->ErrorCode = HAL_UART_ERROR_NONE;
+        HAL_UARTEx_ReceiveToIdle_IT(huart, uart_data, uart_data_size);
+    }
+}
 int main(void)
 {
     init_hardware();
-    motors_init(htim3, htim4);
-    uart_init(&huart3);
-    uart_register_receive_callback(uart_action);
+    HAL_UARTEx_ReceiveToIdle_IT(&huart3, uart_data, uart_data_size); // motors_init(htim3, htim4);
+    // uart_init(&huart3);
+    // uart_register_receive_callback(uart_action);
+
+    // // 1. Start the UART DMA reception
+    // // This should be called in your main() or init function
+    // HAL_StatusTypeDef status = HAL_OK;
+    // status = HAL_UARTEx_ReceiveToIdle_DMA(&huart3, rx_buffer, RX_BUF_SIZE);
+    // __HAL_DMA_DISABLE_IT(&hdma_usart3_rx, DMA_IT_HT); // Optional: Disable Half-Transfer interrupt if not needed
 
     /* Init scheduler */
-    osKernelInitialize();
+    osKernelInitialize(); /* Call init function for freertos objects (in cmsis_os2.c) */
     defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
     osKernelStart();
     while (1)
     {
+        osDelay(pdMS_TO_TICKS(5000));
     }
 }
